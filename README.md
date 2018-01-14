f99239c5a8c59735a7fea40e1903fd87a55a4f39
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-01-14 13:31:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+level1 starter application.
+
+Copy this project and use it as a foundation for your game.
+
+`npm install` install dependencies
+
+`npm run l1` install the l1 library from a folder on your computer (specified in package.json)
+
+`npm run start` start the server
+
+`npm run build` continously build the frontend code
+
+`npm run watch` continuously build the frontend code
---
+++ src/client/index.js
@@ -0,0 +1,27 @@
+import 'babel-polyfill';
+// eslint-disable-next-line no-unused-vars
+import { Game, Entity, Timer, Key, Debug, Gamepad, Physics, Sound, Net, Text } from 'l1';
+import sprites from './sprites.json';
+import scanGamepads from './behaviors/scanGamepads';
+
+// import createControllerPresets from './controllerPresets';
+
+Game.init(600, 400, sprites, { debug: true, physics: true }).then(() => {
+  Game.start();
+  Game.getPhysicsEngine().world.gravity.y = 1;
+
+  Debug.toggleHitboxes();
+  // createControllerPresets();
+
+  const input = Entity.create('input');
+  input.behaviors.scan = scanGamepads();
+
+  const square = Entity.create('square');
+  const sprite = Entity.addSprite(square, 'square');
+  Entity.addBody(square, Physics.Bodies.rectangle(100, 10, 80, 80));
+  sprite.scale.set(5);
+  sprite.anchor.y = 0.65;
+
+  const floor = Entity.create('floor');
+  Entity.addBody(floor, Physics.Bodies.rectangle(300, 390, 600, 10, { isStatic: true }));
+});

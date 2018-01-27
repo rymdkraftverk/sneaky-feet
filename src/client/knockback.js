338eb9745dc5ffacbd83575f9429086d5a5b04b3
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2018-01-27 10:16:20 +0100

    Add knockback to projectile

--- src/client/knockback.js
+++ src/client/knockback.js
@@ -4,14 +4,34 @@ import stalk from './behaviors/stalk'
 
 const spriteName = 'square'
 
+const add = Physics.Vector.add
+const sub = Physics.Vector.sub
+const mult = Physics.Vector.mult
+const normalise = Physics.Vector.normalise
+
+const forceFactor = 50
+
 const formatType = projectile =>
   projectile.id
 
 const formatKnockBackTargetType = id =>
   'about to get knocked the fuck out ' + id
 
-const knockBack = (player, projectile) => {
-  Entity.destroy(projectile.entity)
+const knockBack = projectileId => (obj1, obj2) => {
+  const projectile = obj1.entity.id == projectileId
+    ? obj1.entity
+    : obj2.entity
+
+  const target = obj1.entity.id == projectileId
+    ? obj2.entity
+    : obj1.entity
+
+  const forceDirection = normalise(sub(target.body.position, projectile.body.position))
+
+  const velocity = add(target.body.velocity, mult(forceDirection, forceFactor))
+  Physics.Body.setVelocity(target.body, velocity)
+
+  Entity.destroy(projectile)
 }
 
 const createKnockBackProjectile = ({x, y}) => {
@@ -32,7 +52,7 @@ const fireKnockBack = (targetId, {x, y}) => {
   Entity.addCollision(
     formatType(projectile),
     formatKnockBackTargetType(targetId),
-    knockBack
+    knockBack(projectile.id)
   )
 
   projectile.behaviors.stalk = stalk(targetId)

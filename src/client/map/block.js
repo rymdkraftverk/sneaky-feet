7368da963092bcf71201d7f6ca668e10e47bbff5
fabrly <robert.barlin@gmail.com>
2018-01-27 14:11:51 +0100

    platform block ui

--- src/client/map/block.js
+++ src/client/map/block.js
@@ -3,19 +3,19 @@ import { categories } from '../collisions'
 
 const type = 'map_block'
 const id = i => `${type}_${i}`
-const spriteName = 'base-platform-block'
+const spriteName = 'base-platform-block-1'
 const height = 20
 const width = 45
 
 const init = (i, { x, y }) => {
   const block = Entity.create(id(i))
   Entity.addSprite(block, spriteName)
-  Entity.addBody(block, Physics.Bodies.rectangle(x, y, width, height, { 
+  Entity.addBody(block, Physics.Bodies.rectangle(x, y, width, height, {
     isStatic: true,
     collisionFilter: {
       category: categories.default,
       mask: categories.default | categories.characters,
-    }, 
+    },
   }))
   Entity.addType(block, type)
 
--- src/client/map/floor_block.js
+++ src/client/map/floor_block.js
@@ -2,11 +2,11 @@ import { Entity, Physics } from 'l1'
 
 const type = 'floor_block'
 const id = i => `${type}_${i}`
-const spriteName = 'square'
+const spriteName = 'base-platform-block-1'
 const height = 20
 const width = 45
 
-const init = (i, {x, y}) => {
+const init = (i, { x, y }) => {
   const block = Entity.create(id(i))
   const sprite = Entity.addSprite(block, spriteName)
   Entity.addBody(block, Physics.Bodies.rectangle(x, y, width, height, { isStatic: true }))

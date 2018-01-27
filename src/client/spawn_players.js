36f9ecf9e386993bd1673816f6748c6457970517
Lorentz Lasson <lasson.lorentz@gmail.com>
2018-01-27 11:05:29 +0100

    Assign random targets

--- src/client/spawn_players.js
+++ src/client/spawn_players.js
@@ -1,48 +1,55 @@
+import _ from 'lodash'
+
 import { initPlayer, player1Animation, player2Animation, player3Animation, player4Animation } from './player'
 import keyboard from './move/keyboard'
 import gamepad from './move/gamepad'
 
 const onDeath = playerId => () => console.log(playerId + ' has died')
 
-const players = [
+const player_targets = player_ids => {
+  const shuffled = _.shuffle(player_ids)
+  return shuffled.map((id, i) => {
+    const target_index = i === (player_ids.length - 1) ? 0 : i + 1
+    const t = shuffled[target_index]
+    return {[id]: t}
+  })
+}
+
+const player_templates = [
   {
     id: 'player1',
     x: 400,
     y: 100,
-    target: 'player2',
     animation: player1Animation,
   },
   {
     id: 'player2',
     x: 600,
     y: 100,
-    target: 'player3',
     animation: player2Animation,
   },
   {
     id: 'player3',
     x: 800,
     y: 100,
-    target: 'player4',
     animation: player3Animation,
   },
   {
     id: 'player4',
     x: 1500,
     y: 100,
-    target: 'player1',
     animation: player4Animation,
   },
 ]
 
 export default n => {
-  console.log(players)
-  console.log(n)
-  players.slice(0, n).forEach(p => {
-    console.log(p)
+  const players = player_templates.slice(0, n)
+  const targets = player_targets(players.map(x => x.id))
+
+  players.forEach(p => {
     const player = initPlayer(
       p.id,
-      p.target,
+      targets[p.id],
       {x: p.x, y: p.y},
       onDeath(p.id),
       p.animation

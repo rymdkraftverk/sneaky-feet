eed8c74a04d3907d11e45c1413cbe261f6687d11
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-01-27 01:33:57 +0100

    Add some dysfunctional reptiles

--- src/client/player.js
+++ src/client/player.js
@@ -3,12 +3,12 @@ import { Entity, Physics } from 'l1'
 import { initImmolationAura, formatImmolationTargetType } from './immolation-aura'
 import { initHealth } from './health'
 
-const spriteName = 'square'
+const animation = ['lizard1', 'lizard2']
 const playerType = 'playerType'
 
 const createPlayer = (id, {x, y}) => {
   const player = Entity.create(id)
-  const sprite = Entity.addSprite(player, spriteName)
+  const sprite = Entity.addAnimation(player, animation, 0.05, { zIndex: 10 })
   Entity.addBody(player, Physics.Bodies.rectangle(x, y, 80, 80, {
     inertia: Infinity,
   }))

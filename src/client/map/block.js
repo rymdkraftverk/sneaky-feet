e5d6f9a6c5ca29c0e4f43ba66cf0e8d19a9fbb17
fabrly <robert.barlin@gmail.com>
2018-01-27 01:57:57 +0100

    base block

--- src/client/map/block.js
+++ src/client/map/block.js
@@ -2,17 +2,15 @@ import { Entity, Physics } from 'l1'
 
 const type = 'map_block'
 const id = i => `${type}_${i}`
-const spriteName = 'square'
+const spriteName = 'base-platform-block'
 const height = 20
 const width = 45
 
-const init = (i, {x, y}) => {
+const init = (i, { x, y }) => {
   const block = Entity.create(id(i))
   const sprite = Entity.addSprite(block, spriteName)
   Entity.addBody(block, Physics.Bodies.rectangle(x, y, width, height, { isStatic: true }))
   Entity.addType(block, type)
-  sprite.scale.y = 1
-  sprite.scale.x = 2
 
   return block
 }

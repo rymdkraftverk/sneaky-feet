6cb530c6b54e64f77cfc0d3a9ad4eead20f4fb19
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-01-27 11:16:37 +0100

    Fix jumping not working

--- src/client/collisions.js
+++ src/client/collisions.js
@@ -5,13 +5,20 @@ export const categories = {
   characters: 0x0002,
 }
 
+const setOnGround = (body, onGround) => {
+  if (body.entity.types.includes('playerType')) {
+    body.entity.onGround = onGround
+  }
+}
+
+
 export default () => {
-  Entity.addCollision('playerType', ['floor_block'], (bodyA) => {
-    // Player
-    bodyA.entity.onGround = true
+  Entity.addCollision('playerType', ['floor_block'], (bodyA, bodyB) => {
+    setOnGround(bodyA, true)
+    setOnGround(bodyB, true)
   })
-  Entity.addCollision('playerType', ['floor_block'], (bodyA) => {
-    // Player
-    bodyA.entity.onGround = false
+  Entity.addCollision('playerType', ['floor_block'], (bodyA, bodyB) => {
+    setOnGround(bodyA, false)
+    setOnGround(bodyB, false)
   }, 'collisionEnd')
 }

a3e9efc7714e71f6cd364dea037ac0f9ae5eea8e
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-01-27 23:58:18 +0100

    Juicyfy portals

--- src/client/portal.js
+++ src/client/portal.js
@@ -7,7 +7,7 @@ const playerType = 'portal'
 const createPortal = (id, x, y) => {
   const entity = Entity.create(id)
   entity.portalEnabled = true
-  const sprite = Entity.addAnimation(entity, animation, 0.05, { zIndex: 10 })
+  const sprite = Entity.addAnimation(entity, animation, 0.05, { zIndex: -10 })
   Entity.addType(entity, playerType)
   Entity.addBody(entity, Physics.Bodies.rectangle(x, y, 60, 80, { isStatic: true, isSensor: true, category: categories.characters } ))
   sprite.x = x
@@ -17,49 +17,12 @@ const createPortal = (id, x, y) => {
   return entity
 }
 
-const getObjects = (a, b) => {
-  if (a.entity.types.includes('portal')) {
-    return { portal: a, player: b }
-  }
-  return { portal: b, player: a }
-}
-
-const portalCollision = (a, b) => {
-  const { portal, player } = getObjects(a, b)
-  if (!portal.entity.portalEnabled) {
-    return
-  }
-
-  portal.entity.portalEnabled = false
-  portal.entity.portalPair.portalEnabled = false
-  Entity.get('portalCreator').portalsActive = false
-
-  const x = portal.entity.portalPair.body.position.x
-  const y = portal.entity.portalPair.body.position.y
-
-  Entity.destroy(portal.entity.portalPair)
-  Physics.Body.setPosition(player.entity.body, { x, y })
-  Entity.destroy(portal.entity)
-}
-
 const createPortalPair = (aId, bId, { ax, ay, bx, by }) => {
   const portalA = createPortal(aId, ax, ay)
   const portalB = createPortal(bId, bx, by)
 
   portalA.portalPair = portalB
   portalB.portalPair = portalA
-
-  Entity.addCollision(
-    'portal',
-    ['playerType'],
-    portalCollision
-  )
-
-  Entity.addCollision(
-    'portal',
-    ['playerType'],
-    portalCollision
-  )
 
   return { portalA, portalB }
 }
--- src/client/portals.js
+++ src/client/portals.js
@@ -1,4 +1,4 @@
-import { Entity, Timer, Util } from 'l1'
+import { Entity, Timer, Util, Physics, Sound } from 'l1'
 import _ from 'lodash'
 import { createPortalPair } from './portal'
 
@@ -9,10 +9,45 @@ const portalPositions = [
   { x: 1200, y: 200},
 ]
 
+const getObjects = (a, b) => {
+  if (a.entity.types.includes('portal')) {
+    return { portal: a, player: b }
+  }
+  return { portal: b, player: a }
+}
+
+const portalCollision = (a, b) => {
+  const { portal, player } = getObjects(a, b)
+  if (!portal.entity.portalEnabled) {
+    return
+  }
+
+  portal.entity.portalEnabled = false
+  portal.entity.portalPair.portalEnabled = false
+  Entity.get('portalCreator').portalsActive = false
+
+  const x = portal.entity.portalPair.body.position.x
+  const y = portal.entity.portalPair.body.position.y
+  
+  const onFinish = () => {
+    Entity.destroy(portal.entity.portalPair)
+    Entity.destroy(portal.entity)
+  }
+  player.entity.behaviors.portalTravelling = portalTravelling(x, y, onFinish) 
+  Sound.getSound('./sound/portal2.wav', { volume: 0.3 }).play()
+}
+
+
 export default () => {
   const portalCreator = Entity.create('portalCreator')
   portalCreator.behaviors.portalCreatorB = portalCreatorB()
   portalCreator.portalsActive = false
+
+  Entity.addCollision(
+    'portal',
+    ['playerType'],
+    portalCollision
+  )
 }
 
 const portalCreatorB = () => ({
@@ -32,6 +67,37 @@ const portalCreatorB = () => ({
         by: portalsToCreate[1].y,
       })
       delete b.timeToNextPortal
+    }
+  },
+})
+
+const portalTravelling = (x, y, onFinish) => ({
+  time: 15,
+  init: (b, e) => {
+    Physics.Body.setStatic(e.body, true)
+    b.originalScale = e.sprite.scale.x
+    b.timeToDisappear = Timer.create(b.time)
+  },
+  run: (b, e) => {
+    if (b.timeToDisappear && e.sprite.scale.x > 0) {
+      e.sprite.scale.x -= 0.5
+    }
+    if (b.timeToDisappear && b.timeToDisappear.run()) {
+      b.timeToAppear = Timer.create(b.time)
+      Physics.Body.setPosition(e.body, { x, y })
+      delete b.timeToDisappear
+    }
+    
+    const scaleFactor = (time) => {
+      return time / b.time
+    }
+    if (!b.timeToDisappear && b.timeToAppear) {
+      e.sprite.scale.x = scaleFactor(b.timeToAppear.counter()) * b.originalScale
+    }  
+    if (b.timeToAppear && b.timeToAppear.run()) {
+      onFinish()
+      delete e.behaviors.portalTravelling
+      Physics.Body.setStatic(e.body, false)
     }
   },
 })

d38cc2c0689f0a73f34b18911b1cdc4a105dac8c
fabrly <robert.barlin@gmail.com>
2018-01-28 10:11:40 +0100

    fix bubble

--- src/client/snige_fodder.js
+++ src/client/snige_fodder.js
@@ -6,24 +6,24 @@ const threshold = 0.999
 export default id => ({
   chat: b => {
     b.chat_remaining--
-    if(b.chat_remaining > 0) return
+    if (b.chat_remaining > 0) return
     b.sprite.visible = false
   },
   init_chat: b => {
-    if(b.sprite.visible) return
+    if (b.sprite.visible) return
     b.sprite.visible = true
     b.chat_remaining = chat_duration
   },
   init: b => {
     const baloon = Entity.create(`baloon_snige_fodder_${id}`)
-    b.sprite = Entity.addSprite(baloon, 'prat-bubble')
+    b.sprite = Entity.addSprite(baloon, 'prat-bubble', { zIndex: 99 })
   },
   run: (b, e) => {
     const r = Math.random()
     b.sprite.x = e.sprite.x - e.sprite.width + 70
     b.sprite.y = e.sprite.y - e.sprite.height - 55
 
-    if(b.sprite.visible) b.chat(b)
-    if(r > threshold) b.init_chat(b)
+    if (b.sprite.visible) b.chat(b)
+    if (r > threshold) b.init_chat(b)
   },
 })
